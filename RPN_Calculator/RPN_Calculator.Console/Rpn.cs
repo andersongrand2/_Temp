2fc07af434f8be685255e763c274c04a06965512
Anderson Rodrigues <andersongrand2@outlook.com>
2015-12-13 20:20:20 -0200

    Reverse Polish notation Calculator

---
+++ RPN_Calculator/RPN_Calculator.Console/Rpn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPN_Calculator.ConsoleApp
+{
+    class Rpn
+    {
+        public static void Main()
+        {
+            char[] sp = new char[] { ' ', '\t' };
+            for (;;)
+            {
+                string s = Console.ReadLine();
+                if (s == null) break;
+                Stack<string> tks = new Stack<string>
+                     (s.Split(sp, StringSplitOptions.RemoveEmptyEntries));
+                if (tks.Count == 0) continue;
+                try
+                {
+                    double r = evalrpn(tks);
+                    if (tks.Count != 0) throw new Exception();
+                    Console.WriteLine(r);
+                }
+                catch (Exception e) { Console.WriteLine("error"); }
+            }
+        }
+
+        private static double evalrpn(Stack<string> tks)
+        {
+            string tk = tks.Pop();
+            double x, y;
+            if (!Double.TryParse(tk, out x))
+            {
+                y = evalrpn(tks); x = evalrpn(tks);
+                if (tk == "+") x += y;
+                else if (tk == "-") x -= y;
+                else if (tk == "*") x *= y;
+                else if (tk == "/") x /= y;
+                else throw new Exception();
+            }
+            return x;
+        }
+    }
+}
